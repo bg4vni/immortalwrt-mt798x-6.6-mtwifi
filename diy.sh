019c20b02f52bb0985eb91814bdbf248463e1747
bg4vni <72649376+bg4vni@users.noreply.github.com>
2026-09-26 16:22:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -33,15 +33,25 @@ EOF
 # 1. 将默认IP修改为192.168.100.1
 sed -i 's/192.168.6.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# 2. 将 passwall_dep 强制插入到 feeds.conf.default 的【第一行】（最高优先级）
-sed -i '1i src-git passwall_dep https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-
-# 3. 添加 Nikki 官方软件源
+# 2. 添加 Nikki 官方软件源
 echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
 
-# 4. 添加 PassWall 2 官方源（通过 feeds 机制拉取，会自动匹配系统依赖）
-#echo 'src-git passwall_dep https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+# 3. 添加 PassWall 2 官方源（通过 feeds 机制拉取，会自动匹配系统依赖）
+echo 'src-git passwall_dep https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> feeds.conf.default
+
+# 稀疏抽离并移动到 package/ 一级目录（满足强迫症，100% 确保最高优先级）
+git clone --depth 1 --filter=blob:none --sparse https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/openwrt-passwall-packages
+cd package/openwrt-passwall-packages
+git sparse-checkout set xray-core v2ray-geodata
+
+# 直接把两个子文件夹移出来到上一级（即 package/ 目录）
+mv xray-core ..
+mv v2ray-geodata ..
+
+# 退回根目录并清理空目录
+cd ../..
+rm -rf package/openwrt-passwall-packages
 
 # 5. 添加luci-adguardhome软件源
 echo 'src-git adguardhome https://github.com/kenzok8/openwrt-packages.git;master' >> feeds.conf.default
